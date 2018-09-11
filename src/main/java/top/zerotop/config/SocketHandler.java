1750e560f445db7e546a6104f08a24a262015601
0top <qk122.5.25@icloud.com>
2018-09-11 21:28:50 +0800

    add websocket support

---
+++ src/main/java/top/zerotop/config/SocketHandler.java
@@ -0,0 +1,46 @@
+package top.zerotop.config;
+
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by jupeng.wang on 2018/9/11.
+ */
+public class SocketHandler extends TextWebSocketHandler {
+
+    public final static List<WebSocketSession> socketSession = Collections.synchronizedList(new ArrayList<>());
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        System.out.println("get message "+message.getPayload());
+
+        for (WebSocketSession websession : socketSession) {
+            websession.sendMessage(new TextMessage("收到新消息"+message.getPayload()));
+        }
+
+
+        super.handleTextMessage(session, message);
+    }
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        if (session.isOpen()) {
+            socketSession.add(session);
+        }
+        session.sendMessage(new TextMessage("id 为 "+ session.getId() + " 用户建立连接"));
+        super.afterConnectionEstablished(session);
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        socketSession.remove(session);
+        super.afterConnectionClosed(session, status);
+    }
+}
+
---
+++ src/main/java/top/zerotop/config/SocketInterceptor.java
@@ -0,0 +1,26 @@
+package top.zerotop.config;
+
+import com.sun.istack.internal.Nullable;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.server.HandshakeInterceptor;
+
+import java.util.Map;
+
+/**
+ * Created by jupeng.wang on 2018/9/11.
+ */
+public class SocketInterceptor implements HandshakeInterceptor {
+
+    @Override
+    public boolean beforeHandshake(ServerHttpRequest serverHttpRequest, ServerHttpResponse serverHttpResponse, WebSocketHandler webSocketHandler, Map<String, Object> map) throws Exception {
+        System.out.println("start shake");
+        return true;
+    }
+
+    @Override
+    public void afterHandshake(ServerHttpRequest serverHttpRequest, ServerHttpResponse serverHttpResponse, WebSocketHandler webSocketHandler, @Nullable Exception e) {
+        System.out.println("end shake");
+    }
+}
